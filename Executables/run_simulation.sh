8c315f2207a13e4b6acb47103677ee7c21d06c3d
tathagatamaiti <tathagata.p.maiti@gmail.com>
2024-07-25 04:58:31 +0200

    added migration with a freq of 1 sec

--- Executables/run_simulation.sh
+++ Executables/run_simulation.sh
@@ -23,73 +23,74 @@ ARRIVAL_RATE_11=22
 ARRIVAL_RATE_12=24
 ARRIVAL_RATE_13=26
 MU=0.02
-MIGRATION_CASE=1
+SCALING_CASE=1
+MIGRATION_FREQUENCY=1000
 OUTPUT_FILE="simulation"
 SEED=42
 
 # Run 1
 echo "Running simulation with lambda=${ARRIVAL_RATE_1}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${DEFAULT_RUN_ID} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_1} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_1}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${DEFAULT_RUN_ID} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_1} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_1}.log --seed ${SEED}
 
 # Run 2
 NEW_RUN_ID_2=2
 echo "Running simulation with lambda=${ARRIVAL_RATE_2}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_2} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_2} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_2}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_2} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_2} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_2}.log --seed ${SEED}
 
 # Run 3
 NEW_RUN_ID_3=3
 echo "Running simulation with lambda=${ARRIVAL_RATE_3}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_3} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_3} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_3}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_3} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_3} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_3}.log --seed ${SEED}
 
 # Run 4
 NEW_RUN_ID_4=4
 echo "Running simulation with lambda=${ARRIVAL_RATE_4}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_4} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_4} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_4}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_4} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_4} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_4}.log --seed ${SEED}
 
 # Run 5
 NEW_RUN_ID_5=5
 echo "Running simulation with lambda=${ARRIVAL_RATE_5}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_5} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_5} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_5}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_5} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_5} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_5}.log --seed ${SEED}
 
 # Run 6
 NEW_RUN_ID_6=6
 echo "Running simulation with lambda=${ARRIVAL_RATE_6}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_6} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_6} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_6}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_6} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_6} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_6}.log --seed ${SEED}
 
 # Run 7
 NEW_RUN_ID_7=7
 echo "Running simulation with lambda=${ARRIVAL_RATE_7}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_7} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_7} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_7}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_7} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_7} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_7}.log --seed ${SEED}
 
 # Run 8
 NEW_RUN_ID_8=8
 echo "Running simulation with lambda=${ARRIVAL_RATE_8}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_8} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_8} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_8}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_8} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_8} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_8}.log --seed ${SEED}
 
 # Run 9
 NEW_RUN_ID_9=9
 echo "Running simulation with lambda=${ARRIVAL_RATE_9}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_9} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_9} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_9}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_9} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_9} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_9}.log --seed ${SEED}
 
 # Run 10
 NEW_RUN_ID_10=10
 echo "Running simulation with lambda=${ARRIVAL_RATE_10}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_10} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_10} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_10}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_10} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_10} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_10}.log --seed ${SEED}
 
 # Run 11
 NEW_RUN_ID_11=11
 echo "Running simulation with lambda=${ARRIVAL_RATE_11}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_11} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_11} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_11}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_11} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_11} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_11}.log --seed ${SEED}
 
 # Run 12
 NEW_RUN_ID_12=12
 echo "Running simulation with lambda=${ARRIVAL_RATE_12}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_12} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_12} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_12}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_12} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_12} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_12}.log --seed ${SEED}
 
 # Run 13
 NEW_RUN_ID_13=13
 echo "Running simulation with lambda=${ARRIVAL_RATE_13}"
-python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_13} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_13} --mu ${MU} --migration_case ${MIGRATION_CASE} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_13}.log --seed ${SEED}
+python3 ../Scripts/Simulation/main_sim.py --run_id ${NEW_RUN_ID_13} --upf_case ${DEFAULT_UPF_CASE} --max-upf-instances ${DEFAULT_MAX_UPF_INSTANCES} --min-upf-instances ${DEFAULT_MIN_UPF_INSTANCES} --max-sessions-per-upf ${DEFAULT_MAX_SESSIONS_PER_UPF} --scale-out-threshold ${SCALE_OUT_THRESHOLD} --scale-in-threshold ${SCALE_IN_THRESHOLD} --simulation-time ${SIMULATION_TIME} --arrival_rate ${ARRIVAL_RATE_13} --mu ${MU} --scaling_case ${SCALING_CASE} --migration_frequency ${MIGRATION_FREQUENCY} --output-file ../Logs/${OUTPUT_FILE}_lambda_${ARRIVAL_RATE_13}.log --seed ${SEED}
 
 # Calculations
 python3 ../Scripts/Simulation/calculations.py
--- Scripts/Simulation/scheduler.py
+++ Scripts/Simulation/scheduler.py
@@ -9,6 +9,7 @@ from upf import UPF
 
 EVENT_GENERATE_PDU_SESSION = 1
 EVENT_TERMINATE_PDU_SESSION = 2
+EVENT_MIGRATE_SESSIONS = 3
 
 
 class Scheduler:
@@ -17,8 +18,8 @@ class Scheduler:
     """
 
     def __init__(self, run_id, upf_case, max_upf_instances, min_upf_instances, max_sessions_per_upf,
-                 scale_out_threshold,
-                 scale_in_threshold, simulation_time, arrival_rate, mu, migration_case, output_file, seed=None):
+                 scale_out_threshold, scale_in_threshold, simulation_time, arrival_rate, mu, scaling_case,
+                 migration_frequency, output_file, seed=None):
         """
         Initialize the scheduler with simulation parameters.
 
@@ -33,7 +34,8 @@ class Scheduler:
         :param simulation_time: Total simulation time.
         :param arrival_rate: Rate of session arrival (λ).
         :param mu: Session duration parameter (µ).
-        :param migration_case: Case for session migration.
+        :param scaling_case: Case for session migration.
+        :param migration_frequency: Frequency of session migration events.
         :param output_file: File to write simulation outputs.
         """
         self.event_queue = []
@@ -48,7 +50,8 @@ class Scheduler:
         self.scale_in_threshold = scale_in_threshold
         self.arrival_rate = arrival_rate
         self.mu = mu
-        self.migration_case = migration_case
+        self.scaling_case = scaling_case
+        self.migration_frequency = migration_frequency
         self.num_upf_instances = 0
         self.next_upf_id = 0
         self.session_counter = 0
@@ -228,98 +231,14 @@ class Scheduler:
             message = (f"Time: {np.ceil(self.current_time)}, PDU Session {session.session_id} "
                        f"terminated on UPF {upf.upf_id}")
             self._log(message)
-            if self.migration_case == 1:
-                # Case 1: No migration and uses scale-in threshold for termination
+            if self.scaling_case == 1:
+                # Case 1: uses scale-in threshold for termination
                 if self.free_slots == self.scale_in_threshold and self.num_upf_instances >= self.min_upf_instances + 1:
                     self.scale_in(upf)
 
-            if self.migration_case == 2:
-                # Case 2: No migration and doesn't use scale-in threshold for termination
+            if self.scaling_case == 2:
+                # Case 2: doesn't use scale-in threshold for termination
                 self.scale_in(upf)
-
-            if self.migration_case == 3:
-                # Case 3: UPFs accept PDUs even after migration and uses scale-in threshold for termination
-                sorted_upfs = sorted(self.upfs, key=lambda x: len(x.sessions), reverse=True)
-                target_upf = next((u for u in sorted_upfs if
-                                   u != upf and len(u.sessions) + len(upf.sessions) <= self.max_sessions_per_upf), None)
-
-                if target_upf:
-                    for s in upf.sessions:
-                        target_upf.add_session(s)
-                        upf.remove_session(s)
-                        message = (f"Time: {np.ceil(self.current_time)}, PDU Session {s.session_id} "
-                                   f"migrated from UPF {upf.upf_id} to UPF {target_upf.upf_id}")
-                        self._log(message)
-
-                if self.free_slots == self.scale_in_threshold and self.num_upf_instances >= self.min_upf_instances + 1:
-                    self.scale_in(upf)
-
-            elif self.migration_case == 4:
-                # Case 4: UPFs accept PDUs even after migration and doesn't use scale-in threshold
-                sorted_upfs = sorted(self.upfs, key=lambda x: len(x.sessions), reverse=True)
-                target_upf = next((u for u in sorted_upfs if
-                                   u != upf and len(u.sessions) + len(upf.sessions) <= self.max_sessions_per_upf), None)
-
-                if target_upf:
-                    for s in upf.sessions:
-                        target_upf.add_session(s)
-                        upf.remove_session(s)
-                        message = (f"Time: {np.ceil(self.current_time)}, PDU Session {s.session_id} "
-                                   f"migrated from UPF {upf.upf_id} to UPF {target_upf.upf_id}")
-                        self._log(message)
-
-                if len(upf.sessions) == 0:
-                    self.scale_in(upf)
-
-            elif self.migration_case == 5:
-                # Case 5: UPFs do not accept PDUs after migration and uses scale-in threshold
-                sorted_upfs = sorted(self.upfs, key=lambda x: len(x.sessions), reverse=True)
-                migrated_upfs = set()
-                target_upf = None
-                for u in sorted_upfs:
-                    if u != upf and u not in migrated_upfs:
-                        if len(u.sessions) + len(upf.sessions) <= self.max_sessions_per_upf:
-                            target_upf = u
-                            break
-
-                if target_upf:
-                    migrated_sessions = []
-                    for s in upf.sessions:
-                        migrated_sessions.append(s)
-                        target_upf.add_session(s)
-                        upf.remove_session(s)
-                        message = (f"Time: {np.ceil(self.current_time)}, PDU Session {s.session_id} "
-                                   f"migrated from UPF {upf.upf_id} to UPF {target_upf.upf_id}")
-                        self._log(message)
-                    migrated_upfs.add(upf)
-
-                if self.free_slots == self.scale_in_threshold and self.num_upf_instances >= self.min_upf_instances + 1:
-                    self.scale_in(upf)
-
-            elif self.migration_case == 6:
-                # Case 6: UPFs do not accept PDUs after migration and doesn't use scale-in threshold
-                sorted_upfs = sorted(self.upfs, key=lambda x: len(x.sessions), reverse=True)
-                migrated_upfs = set()
-                target_upf = None
-                for u in sorted_upfs:
-                    if u != upf and u not in migrated_upfs:
-                        if len(u.sessions) + len(upf.sessions) <= self.max_sessions_per_upf:
-                            target_upf = u
-                            break
-
-                if target_upf:
-                    migrated_sessions = []
-                    for s in upf.sessions:
-                        migrated_sessions.append(s)
-                        target_upf.add_session(s)
-                        upf.remove_session(s)
-                        message = (f"Time: {np.ceil(self.current_time)}, PDU Session {s.session_id} "
-                                   f"migrated from UPF {upf.upf_id} to UPF {target_upf.upf_id}")
-                        self._log(message)
-                    migrated_upfs.add(upf)
-
-                if len(upf.sessions) == 0:
-                    self.scale_in(upf)
 
     def scale_out(self):
         """
@@ -349,6 +268,37 @@ class Scheduler:
         self.log_utilization()
         message = f"Time: {np.ceil(self.current_time)}, Compute Node terminates UPF {upf.upf_id}"
         self._log(message)
+
+    def migrate_sessions(self):
+        """
+        Migrate sessions from UPFs with more free slots to those with fewer free slots.
+        Terminate empty UPFs after migration.
+        """
+        self._log(f"Time: {np.ceil(self.current_time)}, Migration event triggered")
+
+        upfs_sorted_by_free_slots = sorted(self.upfs, key=lambda x: self.max_sessions_per_upf - len(x.sessions),
+                                           reverse=True)
+
+        for upf_with_free_slots in upfs_sorted_by_free_slots:
+            if len(upf_with_free_slots.sessions) == 0:
+                continue
+            for upf_with_less_free_slots in upfs_sorted_by_free_slots[::-1]:
+                if upf_with_free_slots == upf_with_less_free_slots:
+                    continue
+                while len(upf_with_free_slots.sessions) > 0 and len(
+                        upf_with_less_free_slots.sessions) < self.max_sessions_per_upf:
+                    session_to_migrate = upf_with_free_slots.sessions.pop()
+                    upf_with_less_free_slots.add_session(session_to_migrate)
+                    message = (f"Time: {np.ceil(self.current_time)}, PDU Session {session_to_migrate.session_id} "
+                               f"migrated from UPF {upf_with_free_slots.upf_id} to UPF {upf_with_less_free_slots.upf_id}")
+                    self._log(message)
+                    if len(upf_with_free_slots.sessions) == 0:
+                        break
+
+        for upf in upfs_sorted_by_free_slots:
+            if (len(upf.sessions) == 0 and self.free_slots == self.scale_in_threshold
+                    and self.num_upf_instances > self.min_upf_instances + 1):
+                self.scale_in(upf)
 
     def run(self):
         """
@@ -412,6 +362,11 @@ class Scheduler:
         generation_event = Event(EVENT_GENERATE_PDU_SESSION, initial_generation_time)
         heapq.heappush(self.event_queue, generation_event)
 
+        # Schedule the first migration event
+        initial_migration_time = self.migration_frequency
+        migration_event = Event(EVENT_MIGRATE_SESSIONS, initial_migration_time)
+        heapq.heappush(self.event_queue, migration_event)
+
         while self.event_queue and np.ceil(self.current_time) < self.simulation_time:
             event = heapq.heappop(self.event_queue)
             self.current_time = event.time
@@ -454,6 +409,14 @@ class Scheduler:
                 if session:
                     self.terminate_pdu_session(session)
 
+            elif event.event_type == EVENT_MIGRATE_SESSIONS:
+                self.migrate_sessions()
+                # Schedule the next migration event
+                next_migration_time = np.ceil(self.current_time + self.migration_frequency)
+                if next_migration_time <= self.simulation_time:
+                    migration_event = Event(EVENT_MIGRATE_SESSIONS, next_migration_time)
+                    heapq.heappush(self.event_queue, migration_event)
+
         for session_id, rejection_time in self.rejected_sessions:
             rejected_sessions_writer.writerow([rejection_time, session_id])
 
@@ -481,7 +444,6 @@ class Scheduler:
         sim_data = open(f'../Data/sim_data_{self.run_id}.csv', 'w', newline='')
         sim_data_writer = csv.writer(sim_data)
         sim_data_writer.writerow(
-                ['Total PDU sessions processed', 'Rejected sessions', 'Accepted sessions'])
+            ['Total PDU sessions processed', 'Rejected sessions', 'Accepted sessions'])
         sim_data_writer.writerow([self.session_counter, len(self.rejected_sessions),
                                   self.session_counter - len(self.rejected_sessions)])
-
